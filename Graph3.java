160ce6331abf7a9962c6b0a1a2159006e52f92df
vanthanh3132001 <71707359+vanthanh3132001@users.noreply.github.com>
2021-05-26 11:48:34 +0700

    Update Graph3.java

--- Graph3.java
+++ Graph3.java
@@ -1,16 +1,53 @@
 package Graph3;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.geom.Point3;
+import org.graphstream.ui.graphicGraph.GraphicElement;
+import org.graphstream.ui.swing_viewer.SwingViewer;
+import org.graphstream.ui.swing_viewer.ViewPanel;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.camera.Camera;
+import org.graphstream.ui.view.util.InteractiveElement;
 
-import org.graphstream.graph.*;
-import org.graphstream.graph.implementations.*;
-import java.lang.*;
-import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Scanner;
 
-public class Graph3 {
-    public static void main(String[] args) {
+
+
+public class Graph3 extends JFrame implements ActionListener {
+    private JTextArea txtInput;
+    private JTextField tHead ,tTail;
+
+    private JButton find,reset,back;
+
+
+
+
+    public static void main(String args[]) {
+
+
+        EventQueue.invokeLater(new Graph3()::display);
+    }
+
+    private  void  display(){
+
+
+
+
+
+
+
         System.setProperty("org.graphstream.ui", "swing");
-        Graph graph = new SingleGraph("Graphxyz");
+        Graph graph = new SingleGraph("Graphxyz", false, true);
+        graph.setStrict(false);
+        graph.setAutoCreate(true);
         graph.setAttribute("ui.stylesheet", "graph {\n" +
                 "\tfill-color: white;\n" +
                 "}\n" +
@@ -22,32 +59,355 @@ public class Graph3 {
                 "\tstroke-mode: plain;\n" +
                 "\tstroke-color: black;\n" +
                 "}\n" +
+                "node.marked {" +
+                "fill-color: yellow;" +
+                "}"+
+                "node.marked2 {" +
+                        "fill-color: white;" +
+                        "}"+
+
+
                 "edge {\n" +
                 "\tfill-color: black;\n" +
                 "\tshape: line;\n" +
-                "}\n" );
+                "}\n"+"edge.marked {" +
+                "fill-color: red;" +
+                "}"+"edge.marked2 {" +
+                "fill-color: black;" +
+                "}" );
         Scanner sc = new Scanner(System.in);
         List<String> list = new ArrayList<String>();
+        List<String> list2 = new ArrayList<String>();
+        List<Integer> listBack = new ArrayList<Integer>();
         Integer x1,x2;
         String s1,s2,s;
         boolean ch1,ch2;
         int u,z;
         DFS g = new DFS(100);
+
+
         while (true) {
+
             s1 = sc.next();
             if (s1.equals("Draw")){
+                JFrame frame = new JFrame();
+               BorderLayout layoutManager = new BorderLayout();
+                JPanel panel = new JPanel(layoutManager){
+                    public Dimension getPreferredSize() {
+                        return new Dimension(1200, 600);
+                    }
+                };
+
+                panel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+
+
+              panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+               panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setTitle("TEAM4_PROJECT");
+
+                // Set margins around control in layout.
+                layoutManager.setHgap(0);
+                layoutManager.setVgap(0);
+                // Create buttons.
+                JLabel lHead = new JLabel("Head");
+                 tHead = new JTextField(10);
+                JLabel lTail = new JLabel("Tail");
+                tTail = new JTextField(10);
+                 find = new JButton("Find");
+                 reset = new JButton("Reset graph");
+                back = new JButton("Back");
+               // cbHandler = new CalculateButtonHandler();
+               // find.addActionListener(cbHandler);
+                // Add the buttons to panel.
+
                 for (Node node : graph) {
                     node.setAttribute("ui.label", node.getId());
+
                 }
-                System.out.print("Start & End: ");
-                u = sc.nextInt();
-                z = sc.nextInt();
-                System.out.println("All paths: ");
-                g.printAllPaths(u, z);
-                graph.display();
+              //  System.out.print("Start & End: ");
+              //  u = sc.nextInt();
+               // z = sc.nextInt();
+               // System.out.println("All paths: ");
+               // g.printAllPaths(u,z);
+
+
+
+
+
+
+                SwingViewer view = new SwingViewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+                view.enableAutoLayout();
+
+                ViewPanel viewPanel = (ViewPanel)view.addDefaultView(false);
+
+                viewPanel.removeMouseListener(viewPanel.getMouseListeners()[0]);
+
+                viewPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+
+                Camera camera = view.getDefaultView().getCamera();
+                camera.setAutoFitView(true);
+
+                viewPanel.addMouseMotionListener(new MouseMotionListener() {
+
+                    private int preX = -1;
+                    private int preY = -1;
+
+                    @Override
+                    public void mouseDragged(MouseEvent mouseEvent) {
+                        int currentX = mouseEvent.getX();
+                        int currentY = mouseEvent.getY();
+
+                        Point3 pointView = camera.getViewCenter();
+
+                        if (preX != -1 && preY != -1) {
+                            if (preX < currentX) {
+                                pointView.x -= 0.01;
+                            }
+                            else if (preX > currentX) {
+                                pointView.x += 0.01;
+                            }
+
+                            if (preY < currentY) {
+                                pointView.y += 0.01;
+                            }
+                            else if (preY > currentY) {
+                                pointView.y -= 0.01;
+                            }
+                        }
+                        camera.setViewCenter(pointView.x, pointView.y, pointView.z);
+
+                        preX = currentX;
+                        preY = currentY;
+                    }
+
+                    @Override
+                    public void mouseMoved(MouseEvent mouseEvent) {
+                        GraphicElement node =  ((View) viewPanel).findGraphicElementAt(EnumSet.of(InteractiveElement.NODE), mouseEvent.getX(), mouseEvent.getY());
+                        if (node != null) {
+                            viewPanel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                        }
+                        else {
+                            viewPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                        }
+                    }
+                });
+
+                viewPanel.addMouseWheelListener(new MouseWheelListener() {
+                    @Override
+                    public void mouseWheelMoved(MouseWheelEvent mouseWheelEvent) {
+                        if (mouseWheelEvent.getWheelRotation() < 0) {
+                            camera.setViewPercent(camera.getViewPercent() * 0.95);
+                        }
+                        else {
+                            camera.setViewPercent(camera.getViewPercent() * 1.05);
+                        }
+                    }
+                });
+
+                viewPanel.addMouseListener(new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent mouseEvent) {
+                        GraphicElement node = ((View) viewPanel).findGraphicElementAt(EnumSet.of(InteractiveElement.NODE), mouseEvent.getX(), mouseEvent.getY());
+                        if (node != null) {
+                            System.out.println(node.getId());
+                            graph.getNode(node.getId()).setAttribute("ui.class", "marked");
+                        }
+                    }
+                    @Override
+                    public void mousePressed(MouseEvent mouseEvent) {
+
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent mouseEvent) {
+
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent mouseEvent) {
+
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent mouseEvent) {
+                    }
+                });
+
+                panel.add(viewPanel);
+                frame.add(lHead);
+                frame.add(tHead);
+                frame.add(lTail);
+                frame.add(tTail);
+                frame.add(back);
+                frame.add(reset);
+                frame.add(find);
+
+
+
+
+                txtInput = new JTextArea(8,16);
+                txtInput.setText("All path!\n");
+                JScrollPane jsp = new JScrollPane(txtInput);
+                //jsp.setViewportView(find);
+                txtInput.setEditable(false);
+                frame.add(jsp,BorderLayout.LINE_END);
+
+              /*  g.printAllPaths(1,6);
+                g.returnString();
+                g.clp(); */
+
+
+                back
+
+                        .addActionListener(e-> {
+
+                            g.printAllPaths(Integer.parseInt(tHead.getText()),Integer.parseInt(tTail.getText()));
+
+                            int[] List =new int[g.getValue().size()];
+                            for(int i=0;i<g.getValue().size();i++){
+                                List[i]=(int)(g.getValue().get(i));
+                            };
+
+
+
+                            for(int i=0;i<g.getValue().size();i++){
+                                listBack.add(List[i]);
+                            };
+                            for(int i=0;i<listBack.size();i++){
+                               System.out.println(listBack.get(i));
+                            };
+                            listBack.remove(listBack.size()-1);
+                            for(int i=0;i<listBack.size();i++){
+                                System.out.println(listBack.get(i));
+                            };
+                            System.out.println(listBack.size());
+
+
+
+
+
+                            g.clearString();
+                            g.clp();
+                        });
+
+
+
+
+
+
+
+
+                find
+
+                        .addActionListener(e-> {
+
+                            g.printAllPaths(Integer.parseInt(tHead.getText()),Integer.parseInt(tTail.getText()));
+
+                            txtInput.setText(
+                                     g.getString() +"\nDuong ngan nhat\n "+ g.getValue() );
+
+
+
+                            String text = txtInput.getText();
+                            txtInput.setCaretPosition(text != null ? text.length() : 0);
+
+                          //  System.out.println(g.getValue().get(0));
+                          // System.out.println(g.getValue().size());
+                          //  System.out.println(g.getValue().size());
+
+                            int[] List =new int[g.getValue().size()];
+                            for(int i=0;i<g.getValue().size();i++){
+                               List[i]=(int)(g.getValue().get(i));
+                            };
+
+                            Timer timer = new Timer(1000, new ActionListener() {
+                                int cnt = 0;
+                                @Override
+                                public void actionPerformed(ActionEvent actionEvent) {
+                                    if(cnt == List.length) {
+                                        ((Timer) actionEvent.getSource()).stop();
+                                        return;
+                                    }
+
+                                    graph.getNode(""+List[cnt]+"").setAttribute("ui.class", "marked");
+
+                                    graph.getEdge(""+List[cnt]+List[cnt+1]+"").setAttribute("ui.class", "marked");
+                                    cnt ++;
+
+                                }
+                            });
+                            timer.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            g.clp();
+
+                        });
+
+
+
+
+                reset
+
+
+
+                        .addActionListener(ex-> {
+
+
+                          //  String[] edgeUpdates = {"12", "23","34"};
+
+
+
+                            for(int i=0;i<list.size();i++){
+                                graph.getNode(list.get(i)).setAttribute("ui.class", "marked2");
+                            };
+
+                            for(int i=0;i<list2.size();i++){
+                                graph.getEdge(list2.get(i)).setAttribute("ui.class", "marked2");
+                            };
+
+
+
+
+                        });
+
+
+
+
+
+
+
+               // btnBottom.addActionListener(this);
+
+              //  find.addActionListener(this);
+              //  this.validate();
+
+                frame.add(panel);
+                frame.pack();
+                frame.setLayout(new FlowLayout());
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                frame.setSize(1200, 600);
+
                 break;
             }
             else {
+
                 ch1 = true;
                 ch2 = true;
                 s2 =sc.next();
@@ -67,8 +427,28 @@ public class Graph3 {
                 x2 = Integer.parseInt(s2);
                 g.addEd(x1,x2);
                 s = s1 + s2;
+                list2.add(s);
                 graph.addEdge(s,s1,s2,true);
+
+
             }
+
+
+
+
         }
+
+
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+    // @Override
+
+
+
 }
